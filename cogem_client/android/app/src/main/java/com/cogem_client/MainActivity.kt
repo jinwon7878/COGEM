56f2cb4545b0019743e6c9e5a62c588ce8bc36ef
yoonjinwon <loveyjw78@gmail.com>
2023-12-11 07:40:56 +0900

    splash android

--- cogem_client/android/app/src/main/java/com/cogem_client/MainActivity.kt
+++ cogem_client/android/app/src/main/java/com/cogem_client/MainActivity.kt
@@ -4,8 +4,15 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen
+import android.os.Bundle
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this)
+    super.onCreate(savedInstanceState)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -18,5 +25,5 @@ class MainActivity : ReactActivity() {
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+      DefaultReactActivityDelegate(this, getMainComponentName(), fabricEnabled)
 }
